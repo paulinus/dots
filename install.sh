7ec7a9951851eec05bc3801bce3e6cb13e70fb46
Pau Gargallo <pau.gargallo@gmail.com>
2012-09-12 11:22:09 +0200

    Link files instead of copying.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp -i -v files/.* ~
+FILES=".alias .bash_profile .bashrc .gitconfig .vimrc"
+
+for file in $FILES; do
+  mv ~/$file ~/$file.bak
+  ln -s `pwd`/files/$file ~/$file
+done
+
